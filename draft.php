4b36c7a4b69df88fb12ea675d232acbfb7d1ecf6
dakamin1 <43013588+dakamin1@users.noreply.github.com>
2018-10-30 22:18:27 -0400

    Update draft.php

--- draft.php
+++ draft.php
@@ -8,6 +8,10 @@
     $dbh = new PDO($config['dsn'], $config['username'], $config['password']);
     $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	
+	if(isset($_POST["nameP"])){
+		echo $_POST["nameP];
+	}
+	
 	echo "<h1>Player</h1>";
 	echo "<table style='width:45%; float:left' border='1'>";
 	echo "<TR>";
@@ -20,25 +24,32 @@
 	echo "<TH> Assists </TH>";
 	echo "<TH> Points </TH>";
 	echo "<TH> Penlty Minutes </TH>";
+	echo "<TH> </TH>";
 	echo "</TR>";
 	
 	foreach( $dbh->query("select * from playerLifetime") as $rows){
-		echo "<TR>";
-		echo "<TH>" .$rows[0]."</TH> ";
-		echo "<TH>" .$rows[1]."</TH> ";
-		echo "<TH>" .$rows[2]."</TH> ";
-		echo "<TH>" .$rows[3]."</TH> ";
-		echo "<TH>" .$rows[4]."</TH> ";
-		echo "<TH>" .$rows[5]."</TH> ";
-		echo "<TH>" .$rows[6]."</TH> ";
-		echo "<TH>" .$rows[7]."</TH> ";
-		echo "<TH>" .$rows[8]."</TH> ";
-		echo "</TR>"; 
+		if(!$row[9]){
+			echo "<TR>";
+			echo "<TH>" .$rows[0]."</TH> ";
+			echo "<TH>" .$rows[1]."</TH> ";
+			echo "<TH>" .$rows[2]."</TH> ";
+			echo "<TH>" .$rows[3]."</TH> ";
+			echo "<TH>" .$rows[4]."</TH> ";
+			echo "<TH>" .$rows[5]."</TH> ";
+			echo "<TH>" .$rows[6]."</TH> ";
+			echo "<TH>" .$rows[7]."</TH> ";
+			echo "<TH>" .$rows[8]."</TH> ";
+			echo '<form action="draft.php" method="post">';
+			echo '<input type="hidden" name="nameP" value="'.$row[0].'">';
+			echo '<TD> <input type="submit" name="select2" value="Pick"> </TD>';
+			echo '</form>';
+			echo "</TR>"; 
+		}
 	}
 	echo "</table>";
 
 	echo "<h1>Goalie</h1>";
-	echo "<table style='width:45%; float:left' border='1'>";
+	echo "<table style='width:45%; float:right' border='1'>";
 	echo "<TR>";
 	echo "<TH> Player Name </TH> ";
 	echo "<TH> Team Name </TH>";
@@ -51,18 +62,24 @@
 	echo "<TH> Penlty Minutes </TH>";
 	echo "</TR>";
 	
-	foreach( $dbh->query("select * from playerLifetime") as $rows){
-		echo "<TR>";
-		echo "<TH>" .$rows[0]."</TH> ";
-		echo "<TH>" .$rows[1]."</TH> ";
-		echo "<TH>" .$rows[2]."</TH> ";
-		echo "<TH>" .$rows[3]."</TH> ";
-		echo "<TH>" .$rows[4]."</TH> ";
-		echo "<TH>" .$rows[8]."</TH> ";
-		echo "<TH>" .$rows[6]."</TH> ";
-		echo "<TH>" .$rows[7]."</TH> ";
-		echo "<TH>" .$rows[5]."</TH> ";
-		echo "</TR>"; 
+	foreach( $dbh->query("select * from playerGoalie") as $rows){
+		if(!$row[9]){
+			echo "<TR>";
+			echo "<TH>" .$rows[0]."</TH> ";
+			echo "<TH>" .$rows[1]."</TH> ";
+			echo "<TH>" .$rows[2]."</TH> ";
+			echo "<TH>" .$rows[3]."</TH> ";
+			echo "<TH>" .$rows[4]."</TH> ";
+			echo "<TH>" .$rows[8]."</TH> ";
+			echo "<TH>" .$rows[6]."</TH> ";
+			echo "<TH>" .$rows[7]."</TH> ";
+			echo "<TH>" .$rows[5]."</TH> ";
+			echo '<form action="draft.php" method="post">';
+			echo '<input type="hidden" name="nameG" value="'.$row[0].'">';
+			echo '<TD> <input type="submit" name="select2" value="Pick"> </TD>';
+			echo '</form>';
+			echo "</TR>";
+		}			
 	}
 	echo "</table>";
 	
