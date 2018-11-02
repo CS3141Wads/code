177816f3f5518ac56b64cf666e3c161672b5cd6b
Ann Ciesla <aeciesla@colossus.it.mtu.edu>
2018-11-02 12:51:52 -0400

    Added game page

--- game.php
+++ game.php
@@ -1,4 +1,58 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<?php
+  session_start();
+ ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A layout example that shows off a responsive product landing page.">
+    <title>Gameplay</title>
+    
+    <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-" crossorigin="anonymous">
+    
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-old-ie-min.css">
+    <![endif]-->
+    <!--[if gt IE 8]><!-->
+        <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css">
+    <!--<![endif]-->
+    
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+	
+	<link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/grids-responsive-min.css">
+    
+        <!--[if lte IE 8]>
+            <link rel="stylesheet" href="css/layouts/marketing-old-ie.css">
+        <![endif]-->
+        <!--[if gt IE 8]><!-->
+            <link rel="stylesheet" href="marketing.css">
+        <!--<![endif]-->
+</head>
+<body>
+
+<div class="header">
+    <div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+        <a class="pure-menu-heading" href="">Fantasy Broomball</a>
+
+        <ul class="pure-menu-list">
+			<li class="pure-menu-item"><a href="profile.php" class="pure-menu-link">Profile</a></li>
+			<li class="pure-menu-item"><a href="draft.php" class="pure-menu-link">Draft</a></li>
+			<li class="pure-menu-item"><a href="game.php" class="pure-menu-link">Gameplay</a></li>
+			<li class="pure-menu-item"><a href="logout.php" class="pure-menu-link">Logout</a></li>
+        </ul>
+    </div>
+</div>
+
+</br></br></br>
+
+<div class="pure-g">
+	<div class="pure-u-1-2">
+		<p>Left half of the screen</p>
+	</div>
+	<div class="pure-u-1-2">
+		<button type="submit" name="playGame" value="play" class="pure-button pure-button-primary">Play a Game</button>
+	</div>
+</div>
 
 </html>
