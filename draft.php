efd37c1f229539684790909c08861ac415781e2f
aciesla4 <aeciesla@colossus.it.mtu.edu>
2018-11-07 12:14:08 -0500

    Re-added scroll

--- draft.php
+++ draft.php
@@ -8,10 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="A layout example that shows off a responsive product landing page.">
 	<style>
-	html {
-		overflow-y: hidden; 
-	}
-	
     /* Style the tab */
     .tab {
      overflow: hidden;
