c15aafdb7ba05a3ca8c3a703c01d50e12cbee20b
aciesla4 <aeciesla@colossus.it.mtu.edu>
2018-11-26 15:34:44 -0500

    Updated triggers to handle ties

--- triggers.sql
+++ triggers.sql
@@ -1,7 +1,6 @@
 delimiter //
 create trigger update_team_stats after insert on game
-for each row
-begin
+for each row begin
 	declare win varchar(25); 
     declare dat date; 
     declare t1 varchar(25);
@@ -10,13 +9,29 @@ begin
     select data into dat from game order by data desc limit 1; 
     select team1 into t1 from game where data=dat and team1 != win;
     select team2 into t2 from game where data=dat and team2 != win; 
-    update Team set wins = wins + 1 where name = win; 
-	update Team set losses = losses + 1 where name = ifnull ( t1, t2 ); 
+    if ( win like 'tie' ) then 
+		update Team set ties = ties + 1 where name = t1;
+        update Team set ties = ties + 1 where name = t2; 
+    else 
+		update Team set wins = wins + 1 where name = win; 
+		update Team set losses = losses + 1 where name = ifnull ( t1, t2 ); 
+	end if; 
 end // 
 
+create trigger update_lifetime_stats after update on Team
+for each row begin 
+    if ( NEW.ties = OLD.ties + 1 ) then 
+		update playerLifetime set ties = ties + 1 where name = NEW.player1;
+        update playerLifetime set ties = ties + 1 where name = NEW.player2; 
+        update playerLifetime set ties = ties + 1 where name = NEW.player3;
+        update playerLifetime set ties = ties + 1 where name = NEW.player4; 
+        update playerLifetime set ties = ties + 1 where name = NEW.player5; 
+        update goalieLifetime set ties = ties + 1 where name = NEW.goalie;
+	end if; 
+end //
+
 CREATE TRIGGER update_team_score AFTER UPDATE ON draftedPlayer
-FOR EACH ROW
-BEGIN 
+FOR EACH ROW BEGIN 
 UPDATE Team SET currentScore = 0;
 UPDATE Team INNER JOIN draftedPlayer ON Team.player1 = draftedPlayer.name SET Team.currentScore = Team.currentScore + draftedPlayer.currentScore;
 UPDATE Team INNER JOIN draftedPlayer ON Team.player2 = draftedPlayer.name SET Team.currentScore = Team.currentScore + draftedPlayer.currentScore;
